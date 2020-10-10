0a585da6efc6c36eede2037de3f7804abf6792ac
Alireza Kazemi <a.kazemi.a2001@gmail.com>
2020-10-10 16:45:18 +0330

    commit

--- Home Work/S 2/Ex 1/src/Practice1.java
+++ Home Work/S 2/Ex 1/src/Practice1.java
@@ -1,14 +1,22 @@
+import java.util.*;
 
 public class Practice1 {
 
     public static void main(String[] args) {
-        student x1 = new student();
-        teacher x2 = new teacher();
-        employee x3 = new employee();
+        float a;
+        float b;
         
-        x1.job();
-        x2.job();
-        x3.job();
+        calculator x1 = new calculator();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number : ");
+        a = input.nextFloat();
+        System.out.print("Enter second number : ");
+        b = input.nextFloat();
+        
+        System.out.println("\n"+a+" + "+b+" = "+x1.addition(a,b));  
+        System.out.println(a+" - "+b+" = "+x1.subtract(a,b));  
+        System.out.println(a+" + "+b+" = "+x1.multiply(a,b));  
+        System.out.println(a+" + "+b+" = "+x1.divide(a,b));  
     }
     
 }
